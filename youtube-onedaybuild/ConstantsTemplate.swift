31f3aa4b3997b11c4451710aed4c0947c94db678
Admin <admin@Admins-MacBook-Pro.local>
2020-09-04 21:52:25 +0530

    modified gitignore.

--- youtube-onedaybuild/ConstantsTemplate.swift
+++ youtube-onedaybuild/ConstantsTemplate.swift
@@ -1,5 +1,5 @@
 //
-//  Constants.swift
+//  ConstantsTemplate.swift
 //  youtube-onedaybuild
 //
 //  Created by Admin on 04/09/20.
@@ -7,11 +7,13 @@
 //
 
 import Foundation
+/*
 
 struct Constants {
     
     static var API_KEY = " "
-    static var PLAYLIST_ID = "UU2D6eRvCeMtcF5OGHf1-trw"
+    static var PLAYLIST_ID = ""
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     
 }
+*/
